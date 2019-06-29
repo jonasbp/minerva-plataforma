3cb3c0e4c05199fde56507b311c0154d77b8cbf0
Ronan Silva <contato.ronansilva@gmail.com>
2019-06-29 19:00:16 -0300

    update design patterns
    add design patterns and variables services

--- models/user.js
+++ models/user.js
@@ -1,33 +1,33 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs') //para o password
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs'); //para o password
 /*
 mongoose.connect('mongodb://localhost/loginapp')
 var db = mongoose.connection;
 não precisa disso se não vai ter duas conexoes com o banco
 */
 const UserSchema = mongoose.Schema({
-    username: {
-        type: String,
-        index: true
-    },
-    password: {
-        type: String,
-    },
-    email: {
-        type: String
-    },
-    name: {
-        type: String
-    }
-})
+  username: {
+    type: String,
+    index: true,
+  },
+  password: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+});
 
 let User = mongoose.model('User', UserSchema);
-User.createUser = function (newUser, callback) {
-    bcrypt.genSalt(10, function (err, salt) {
-        bcrypt.hash(newUser.password, salt, function (err, hash) {
-            newUser.password = hash
-            newUser.save(callback)
-        });
+User.createUser = function(newUser, callback) {
+  bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash(newUser.password, salt, function(err, hash) {
+      newUser.password = hash;
+      newUser.save(callback);
     });
-}
+  });
+};
 module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,60 +1,51 @@
 const express = require('express');
 const router = express.Router();
-const {check, validationResult} = require("express-validator");
+const { check, validationResult } = require('express-validator');
 const User = require('../models/user');
 
 // Register
-router.get('/register', function(req,res){
-    res.render('register');
+router.get('/register', function(req, res) {
+  res.render('register');
 });
 
 // Login
-router.get('/login', function(req,res){
-    res.render('login');
+router.get('/login', function(req, res) {
+  res.render('login');
 });
 
+router.post('/register', function(req, res) {
+  const { name, email, username, password, password_confirm } = req.body;
+  //validation
+  req.checkBody('name', 'Name is required').notEmpty();
+  req.checkBody('email', 'Email is required').notEmpty();
+  req.checkBody('email', 'Email is not valid').isEmail();
+  req.checkBody('username', 'Username is required').notEmpty();
+  req.checkBody('password', 'Password is required').notEmpty();
+  req
+    .checkBody('password_confirm', 'Password do not match')
+    .equals(req.body.password);
 
-router.post('/register', function(req,res){
-    const {name, email, username, password, password2} = req.body;
-    //validation
-    req.checkBody('name','Name is required').notEmpty()
-    req.checkBody('email','Email is required').notEmpty()
-    req.checkBody('email','Email is not valid').isEmail()
-    req.checkBody('username','Username is required').notEmpty()
-    req.checkBody('password','Password is required').notEmpty()
-    req.checkBody('password2','Password do not match').equals(req.body.password)
-    
-    const errors = req.validationErrors()
-    if(errors){
-        req.flash("error_msg", errors);
-        res.render('register',{
-            errors:errors
-        });
-    } else{
-            console.log("Criando Usuario...")
-            let newUser = new User({
-                name: name,
-                email:email,
-                username: username,
-                password: password
-            });
-            User.createUser(newUser,function(err,user){
-                 if(err) throw err;
-                 console.log("Usuario Criado: "+ user);
-            });
-            req.flash('success_msg','Registrado com sucesso');
-            res.redirect('/users/login');
-    }
-})
+  const errors = req.validationErrors();
+  if (errors) {
+    req.flash('error_msg', errors);
+    res.render('register', {
+      errors: errors,
+    });
+  } else {
+    console.log('Criando Usuario...');
+    let newUser = new User({
+      name: name,
+      email: email,
+      username: username,
+      password: password,
+    });
+    User.createUser(newUser, function(err, user) {
+      if (err) throw err;
+      console.log('Usuario Criado: ' + user);
+    });
+    req.flash('success_msg', 'Registrado com sucesso');
+    res.redirect('/users/login');
+  }
+});
 
-/*
-use loginapp
-db.users.find()
-npm install bcrypt
-npm install bcrypt --save
-
-*/
-
-
-
-module.exports = router;                     
+module.exports = router;
