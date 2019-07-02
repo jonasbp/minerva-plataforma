59181a24f694d8e129c99b2482e89f782f86c18e
lucasdenovoa <39407665+lucasdenovoa@users.noreply.github.com>
2019-07-02 09:42:03 -0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -6,19 +6,55 @@ var db = mongoose.connection;
 não precisa disso se não vai ter duas conexoes com o banco
 */
 const UserSchema = mongoose.Schema({
-  username: {
-    type: String,
-    index: true,
-  },
-  password: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  name: {
-    type: String,
-  },
+   username: {
+        type: String,
+        required: true,
+        index: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    password2: {
+        type: String,
+        required: true
+    },
+    endereco: {
+        type: String,
+        required: true
+    },
+    phone_num: {
+        type: Number,
+        required: true
+    },
+    age: {
+        type: Number,
+        required: true
+    },
+    live_with: {
+        type: String,
+        required: true
+    },
+    secret_word: {
+        type: String,
+        required: true
+    },
+    isClient: {
+        type: Boolean,
+        required: true
+    },
+    cpf: {
+        type: String,
+        required: true
+    }
 });
 
 let User = mongoose.model('User', UserSchema);
